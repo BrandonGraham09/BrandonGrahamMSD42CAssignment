f117339b24f760ece1cf64c6d1139b7f713718aa
BrandonGraham09 <brandon.graham.e22880@mcast.edu.mt>
2021-01-05 18:04:27 +0100

    Player Movement and Border

---
+++ CarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+
+    float xMin, xMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //setup the boundaries according to camera
+    private void SetUpMoveBoundaries()
+    {
+        //get the camer from Unity
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+    }
+
+    //Moves player
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //newXPos = current x-postition + difference in x
+        var newXPos = transform.position.x + deltaX;
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        //move the Player to newXPos
+        this.transform.position = new Vector2(newXPos, transform.position.y);
+
+
+    }
+}
