1f36050e8664fd8dc3c30ffd984d3ce02f79fe09
BrandonGraham09 <brandon.graham.e22880@mcast.edu.mt>
2021-01-19 12:47:55 +0100

    Music Player with Singleton

---
+++ CarGame/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Runs even before Start()
+    void Awake()
+    {
+        SetUpSingolton();
+    }
+
+    private void SetUpSingolton()
+    {
+        //GetType(): gets the type of Object attached to this script: Music Player
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            //if there is more than one music player, destroy the last one
+            Destroy(gameObject);
+        }
+        else
+        {
+            //dont destroy the music player when changing scenes
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
